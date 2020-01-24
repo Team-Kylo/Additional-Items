d02c121a4051ef28da3e439934f29a3f38a9e6f8
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-01-23 18:18:29 -0700

    Connected the Schema.

--- database/index.js
+++ database/index.js
@@ -4,3 +4,5 @@ mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrl
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log(`Connected to the db`));
+
+const Item = require('./Item.js');
