7a82fd46496bdc2e4562b48ad94a37941d362a6e
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-01-23 23:34:37 -0700

    Found the problem. I have to find a way to wait for a server response for the pictures before putting them in my db.

--- dummyData/dataSeeder.js
+++ dummyData/dataSeeder.js
@@ -1,37 +1,84 @@
-const db = require('../database');
 const Picsum = require('picsum-photos');
 const faker = require('faker');
+const fetch = require('node-fetch');
 require('dotenv').config();
 
+// I chose this route because it is actually less code then trying to connect to my current database
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true});
+const db = mongoose.connection;
 
-const databaseSeeder = () => {
-  let i = 0;
 
-  db.verifyConnection().
-    then(() => {
-      // create a while loop
-      while ( i < 10 ) {
-        console.log(i);
-        // get fake data
-          // some for seller
+const databaseSeeder = (numOfSellers = 10, numOfItemsPerSeller = 8) => {
+  // let i = 0;
 
-          // some for product
 
-        // get fake picture url
-          // one for seller
-          // one for product
+  // create a while loop for the seller
+  // while ( i < numOfSellers ) {
+    // get fake data for seller
 
-        // create an object
+    // sellerPicture: faker.avatar,
 
-        // send the object to the database
-          // have a catch block for errors
+    let generatedNewItemInfo = {
+      sellerName: faker.internet.userName(),
+      sellerCountry: faker.address.country(),
+      sellerTotalSales: Math.floor(Math.random * 500),
+      sellerJoinDate: faker.date.past(),
+    };
 
-        // increment i by one
-        i++;
-      }
+    // wait for the picture url to be grabbed
+    fetch('https://www.placecage.com/200/202').
+      then((sellerPicture) => {
+      console.log("done");
+      return sellerPicture.blob();
+      });
+      // then(image => )
 
-    })
 
+      // generatedNewItemInfo.sellerPicture = sellerPictureURL;
+      // console.log(generatedNewItemInfo);
+
+      // let j = 0
+      // // while loop for the items
+      // while(j < numOfItemsPerSeller) {
+      //   // some for items
+
+      //   // get fake picture url
+      //     // one for seller
+      //     // one for item
+
+
+
+      //   // send the object to the database
+      //     // have a catch block for errors
+
+      //   // increment i by one
+
+      //   i++;
+
+      // }
+
+
+  // };
 };
+
+const addNewItem = (newItem) => {
+
+  let itemToBeAdded = new Item({
+    itemId: newItem.itemId,
+    itemName: newItem.itemName,
+    itemPrice: newItem.itemPrice,
+    itemPicture: newItem.itemPicture,
+    itemShippingPrice: newItem.itemShippingPrice,
+    sellerPicture: newItem.sellerPicture,
+    sellerName: newItem.sellerName,
+    sellerCountry: newItem.sellerCountry,
+    sellerTotalSales: newItem.sellerTotalSales,
+    sellerJoinDate: newItem.sellerJoinDate,
+  });
+
+  return itemToBeAdded.save();
+}
+
 
 databaseSeeder();
