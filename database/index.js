e0a05e5b857186fb16d5fe7946799cbf12654c1e
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-01-23 22:33:17 -0700

    Set up a function that accepts a new item and saves it to the database.

--- database/index.js
+++ database/index.js
@@ -1,8 +1,30 @@
+// mongoose set up and connection
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true});
-
 const db = mongoose.connection;
+
+// error handling or notification when the database connects
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log(`Connected to the db`));
 
+// schema for item
 const Item = require('./Item.js');
+
+
+module.exports.addNewItem = (newItem) => {
+
+  let itemToBeAdded = new Item({
+    itemId: newItem.itemId,
+    itemName: newItem.itemName,
+    itemPrice: newItem.itemPrice,
+    itemPicture: newItem.itemPicture,
+    itemShippingPrice: newItem.itemShippingPrice,
+    sellerPicture: newItem.sellerPicture,
+    sellerName: newItem.sellerName,
+    sellerCountry: newItem.sellerCountry,
+    sellerTotalSales: newItem.sellerTotalSales,
+    sellerJoinDate: newItem.sellerJoinDate,
+  });
+
+  return itemToBeAdded.save();
+}
