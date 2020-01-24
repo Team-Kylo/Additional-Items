76f0875ad2114961146ede60850de4be1989c17c
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-01-23 18:17:57 -0700

    I went through a bunch of renaming.  Not sure this is the best route.  Schema is set up and ready to be used.

---
+++ database/Item.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+  itemId: Number,
+  itemName: String,
+  itemPrice: Number,
+  itemPicture: String,
+  itemShippingPrice: Number,
+  sellerPicture: String,
+  sellerName: String,
+  sellerCountry: String,
+  sellerTotalSales: Number,
+  sellerJoinDate: Date,
+})
+
+module.exports.Item = mongoose.model('Item', itemSchema);
