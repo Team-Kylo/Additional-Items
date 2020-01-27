a1d8407ac4f838a10bdde42da1d91d5951e2075b
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-01-27 15:50:13 -0700

    Pulled out my seeding function to allow for testing of the data generation.

---
+++ dummyData/seeder.js
@@ -0,0 +1,20 @@
+const { generateData } = require('./dataSeeder');
+const db = require('../database');
+
+const populateDatabase = () => {
+  const fakeData = generateData();
+
+  db.addManyItems(fakeData)
+    .then(() => {
+      console.log('Seeding was successful to the Kylo Database with Items Documents!');
+      // exits the node process
+      process.exit();
+    })
+    .catch((error) => {
+      // console logs the error then excess the node process
+      console.error('Seeding the data had the following error:', error);
+      process.exit();
+    });
+};
+
+populateDatabase();
