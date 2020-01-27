1737cb02e2c521ddbe1974e296840ad14bd378b4
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-01-27 12:52:30 -0700

    Setting up Jest to test my schema for Items.  Most of the configuration has been done at this point.

--- database/Schemas.js
+++ database/Schemas.js
@@ -11,6 +11,6 @@ const itemSchema = new mongoose.Schema({
   sellerCountry: String,
   sellerTotalSales: Number,
   sellerJoinDate: Date,
-})
+});
 
 module.exports.Item = mongoose.model('Item', itemSchema);
--- server/index.js
+++ server/index.js
@@ -1,24 +1,6 @@
-// this is where my app will start
-const express = require('express');
+const app = require('./app');
 require('dotenv').config();
-const db = require('../database');
 
-const app = express();
 const port = process.env.ADDITIONAL_ITEMS_PORT || 3004;
-
-app.use(express.static('./public'));
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/:id', (req, res) => {
-  // need to sanitize this at some point
-  db.getAllSellerItemsExceptCurrentItem(req.params.id)
-    .then((allItems) => {
-      res.status(200).json(allItems);
-    })
-    .catch((error) => {
-      res.status(500).json(error);
-    });
-});
-
 
 app.listen(port, console.log(`Listening on port ${port}`));
