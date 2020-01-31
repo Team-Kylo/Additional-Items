7353ac61ed4440674c76a01464ab0cd8bc3d18b4
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-01-31 13:56:52 -0700

    I broke my server yesterday and didn't realize it.  I fixed and and all the tests are still passing.  Now have hanging tests.

--- database/db.js
+++ database/db.js
@@ -2,14 +2,9 @@ require('dotenv').config();
 // mongoose set up and connection
 const mongoose = require('mongoose');
 
-const db = mongoose.createConnection(process.env.DATABASE_URL, {
+const db = mongoose.connect(process.env.DATABASE_URL, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
-
-// error handling or notification when the database connects
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log('Connected to the db'));
-
 
 module.exports.db = db;
