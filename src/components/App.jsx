d5352b3043b5ccb0db8ba66a1fa47215c5d26c21
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-01-31 18:40:31 -0700

    Added test to check that the component is rendering and that it is recieving a prop.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const App = () => (
+const App = ({ itemId }) => (
   <div className="container">
-    Testing.... Testing.... 123?
+    {itemId}
   </div>
 );
+
+
+App.defaultProps = {
+  itemId: '1',
+};
+
+App.propTypes = {
+  itemId: PropTypes.string,
+};
 
 export default App;
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -8,6 +8,11 @@ import App from './App';
 describe('<App />', () => {
   it('renders to the page', () => {
     const wrap = shallow(<App />);
-    expect(wrap.text()).toEqual('Testing.... Testing.... 123?');
+    expect(wrap.exists('.container')).toEqual(true);
+  });
+
+  it('Accepts a string as the prop', () => {
+    const wrap = shallow(<App />);
+    wrap.setProps({ itemId: '5' });
   });
 });
