62930017872df519e2dfd023b60f1d54e1e4ee5d
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-02-02 23:05:57 -0700

    Everything is working as it should except the buttons.

---
+++ src/components/ItemContainer.jsx
@@ -0,0 +1,82 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable no-plusplus */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import ItemEntry from './ItemEntry';
+
+const itemContainer = ({ allItems, onArrowClickCallback }) => {
+  const ItemContainer = styled.div`
+    border-bottom: 1px solid #E1E3DF;
+    display:flex;
+    padding-bottom: 24px;
+    flex-direction: row;
+  `;
+
+  const ArrowBuffer = styled.div`
+    height: 30%
+  `;
+
+  const ArrowContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+  `;
+
+  const Arrow = styled.button`
+    background: #FFFFFF;
+    margin: 0px;
+    height: 43px;
+    width: 43px;
+    border-radius: 50%;
+    border-width: 0px;
+    font-size: 38px;
+    padding: 0px;
+    box-shadow:
+      0 0 5px 5px rgb(0, 0, 0, 0.07),
+      0 0 0px 0px rgb(0, 0, 0, 0.07);
+    :hover {
+      box-shadow:
+        0 0 5px 5px rgb(0, 0, 0, 0.15),
+        0 0 0px 0px rgb(0, 0, 0, 0.15);
+      cursor: pointer;
+      background-color: #f9f9f9;
+    }
+  `;
+
+  // eslint-disable-next-line consistent-return
+  const onArrowClick = (e) => {
+    const classNameArray = e.target.className.split(' ');
+
+    for (let i = 0; i < classNameArray.length; i++) {
+      if (classNameArray[i] === 'left-arrow') {
+        return onArrowClickCallback(-1);
+      }
+      if (classNameArray[i] === 'right-arrow') {
+        return onArrowClickCallback(1);
+      }
+    }
+  };
+
+  return (
+    <ItemContainer>
+      <ArrowContainer>
+        <ArrowBuffer />
+        <Arrow className="left-arrow" onClick={onArrowClick}>&#8249;</Arrow>
+      </ArrowContainer>
+      {allItems.map((item) => (
+        <ItemEntry key={item.itemId} item={item} />
+      ))}
+      <ArrowContainer>
+        <ArrowBuffer />
+        <Arrow className="right-arrow" onClick={onArrowClick}>&#8250;</Arrow>
+      </ArrowContainer>
+    </ItemContainer>
+  );
+};
+
+itemContainer.propTypes = {
+  allItems: PropTypes.arrayOf(PropTypes.object),
+  onArrowClickCallback: PropTypes.func,
+};
+
+export default itemContainer;
---
+++ src/components/ItemEntry.jsx
@@ -0,0 +1,113 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/require-default-props */
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const itemEntry = ({ item }) => {
+  const {
+    itemName = null,
+    itemPrice = null,
+    itemPicture = null,
+    freeShipping = 'no',
+  } = item;
+
+  const ItemDiv = styled.div`
+    cursor: pointer;
+    flex-direction: column;
+    padding: 9px;
+  `;
+
+  const ItemImage = styled.img`
+    content: url(${itemPicture});
+    height: 174.844px;
+    width: 220.188px;
+  `;
+
+  const ItemName = styled.h2`
+    display: block;
+    font-size: 14px;
+    font-weight: 300;
+    -webkit-font-smoothing: antialiased;
+    text-rendering: optimizeLegibility;
+    margin: 0px;
+    line-height: 1.6;
+    font-family: "Graphik Webfont",-apple-system,BlinkMacSystemFont,"Roboto","Droid Sans","Segoe UI","Helvetica",Arial,sans-serif;
+  `;
+
+  const ItemPrice = styled.span`
+    font-weight: 600;
+    font-size: 16px;
+    font-family: "Graphik Webfont",-apple-system,BlinkMacSystemFont,"Roboto","Droid Sans","Segoe UI","Helvetica",Arial,sans-serif;
+    margin-top: 0px;
+    line-height: 1.6;
+    letter-spacing: normal;
+    color: #222;
+  `;
+
+  const ShippingAndAvailibility = styled.div`
+    display:flex;
+    flex-direction: column;
+  `;
+
+  const FreeShippingBadge = styled.span`
+    font-size: 11px;
+    font-weight: bold;
+    padding: 3px 6px;
+    background: #D4E9D7;
+    border-radius: 15px;
+    display: inline-block;
+    word-break: break-word;
+    line-height: 1;
+  `;
+
+  const FreeShippingElgible = styled.span`
+    font-size: 12px;
+    font-family: "Graphik Webfont",-apple-system,BlinkMacSystemFont,"Roboto","Droid Sans","Segoe UI","Helvetica",Arial,sans-serif;
+    font-weight: 300;
+    line-height: 1.6;
+    letter-spacing: normal;
+  `;
+
+  const freeShippingOption = (shipping) => {
+    switch (shipping) {
+      case 'yes':
+        return (
+          <ItemPrice>
+            ${itemPrice} <FreeShippingBadge>FREE shipping</FreeShippingBadge>
+          </ItemPrice>
+        );
+      case 'elgible':
+        return (
+          <ShippingAndAvailibility>
+            <ItemPrice>${itemPrice}</ItemPrice>
+            <FreeShippingElgible>Free shipping eligible</FreeShippingElgible>
+          </ShippingAndAvailibility>
+        );
+      default:
+        return (
+          <ItemPrice>${itemPrice}</ItemPrice>
+        );
+    }
+  };
+
+
+  return (
+    <ItemDiv>
+      <ItemImage />
+      <ItemName>{itemName}</ItemName>
+      {freeShippingOption(freeShipping)}
+    </ItemDiv>
+  );
+};
+
+itemEntry.propTypes = {
+  item: PropTypes.shape({
+    itemName: PropTypes.string,
+    itemPrice: PropTypes.number,
+    itemPicture: PropTypes.string,
+    freeShipping: PropTypes.string,
+  }),
+};
+
+export default itemEntry;
