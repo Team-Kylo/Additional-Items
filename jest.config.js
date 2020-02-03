db259884bff2c98c5edcb667b6263a828bae2619
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-02-03 12:00:26 -0700

    Removed jsdom from env

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   verbose: true,
   preset: '@shelf/jest-mongodb',
-  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>enzyme.config.js'],
   moduleFileExtensions: ['js', 'jsx'],
   moduleDirectories: ['node_modules'],
